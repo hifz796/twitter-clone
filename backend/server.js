052f6b9c44dccec876f8219be3d155fac2d9352d
Ubuntu <ubuntu@ip-172-31-22-241.ec2.internal>
2026-03-16 11:32:37 +0000

    server.js path issue fixed

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,9 @@ app.use("/api/notifications", notificationRoutes);
 
 /* ------------------ SERVE FRONTEND (PRODUCTION) ------------------ */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
   });
 }
 
